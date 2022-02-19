6c738737a80d81a606863600f209400af6a270a7
zamanjaved52 <83859626+zamanjaved52@users.noreply.github.com>
2022-02-19 18:36:13 +0500

    editing

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -249,32 +249,32 @@
 </head>
 
 <body style="">
-{{--<nav class="navbar navbar-expand-lg  navbar-dark bg-dark shadow-lg fixed-top h-16">--}}
-{{--    <div class="container">--}}
-{{--        <a href="#" class="navbar-brand">--}}
-{{--            <!-- Logo Image -->--}}
-{{--            <img src="{{asset('logo/noname.png')}}" width="45" alt="" class="d-inline-block align-middle mr-2">--}}
-{{--            <!-- Logo Text -->--}}
-{{--            <span class="text- font-weight-bold">NoName</span>--}}
-{{--        </a>--}}
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark shadow-lg fixed-top h-16">
+    <div class="container">
+        <a href="#" class="navbar-brand">
+            <!-- Logo Image -->
+            <img src="{{asset('logo/noname.png')}}" width="45" alt="" class="d-inline-block align-middle mr-2">
+            <!-- Logo Text -->
+            <span class="text- font-weight-bold">NoName</span>
+        </a>
 
-{{--        <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>--}}
+        <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
 
-{{--        <div id="navbarSupportedContent" class="collapse navbar-collapse bg-dark">--}}
-{{--            <ul class="navbar-nav ml-auto -m-4">--}}
-{{--                <li class="nav-item "><a href="{{url('/')}}" class="nav-link">Home <span class="sr-only">(current)</span></a></li>--}}
-{{--                <li class="nav-item"><a href="{{url('about')}}" class="nav-link">About</a></li>--}}
-{{--                <li class="nav-item"><a href="#" class="nav-link">Services</a></li>--}}
-{{--                <li class="nav-item"><a href="#" class="nav-link mr-16">Contact</a></li>--}}
-{{--                <li class="nav-item"><a href="{{ route('cart.list') }}" class="nav-link ">--}}
-{{--                        <i class="fa fa-cart-plus fa fa-xs text-white"></i>--}}
-{{--                        <span class="pl-1 text-white -mt-1 fa fa-xs">  {{ Cart::getTotalQuantity()}}</span>--}}
+        <div id="navbarSupportedContent" class="collapse navbar-collapse bg-dark">
+            <ul class="navbar-nav ml-auto -m-4">
+                <li class="nav-item "><a href="{{url('/')}}" class="nav-link">Home <span class="sr-only">(current)</span></a></li>
+                <li class="nav-item"><a href="{{url('about')}}" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
+                <li class="nav-item"><a href="#" class="nav-link mr-16">Contact</a></li>
+                <li class="nav-item"><a href="{{ route('cart.list') }}" class="nav-link ">
+                        <i class="fa fa-cart-plus fa fa-xs text-white"></i>
+                        <span class="pl-1 text-white -mt-1 fa fa-xs">  {{ Cart::getTotalQuantity()}}</span>
 
-{{--                    </a></li>--}}
-{{--            </ul>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</nav>--}}
+                    </a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 <br><br><br>
 <!-- FOR DEMO PURPOSE-->
 
@@ -378,123 +378,123 @@
 
 
 
-{{--<footer class="footer-section mt-24">--}}
-{{--    <div class="container">--}}
-{{--        <div class="footer-cta pt-5 pb-5">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-xl-4 col-md-4 mb-30">--}}
-{{--                    <div class="single-cta">--}}
-{{--                        <i class="fas fa-map-marker-alt"></i>--}}
-{{--                        <div class="cta-text">--}}
-{{--                            <h4>Find us</h4>--}}
-{{--                            <span>1010 Avenue, sw 54321, chandigarh</span>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-xl-4 col-md-4 mb-30">--}}
-{{--                    <div class="single-cta">--}}
-{{--                        <i class="fas fa-phone"></i>--}}
-{{--                        <div class="cta-text">--}}
-{{--                            <h4>Call us</h4>--}}
-{{--                            <span>9876543210 0</span>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-xl-4 col-md-4 mb-30">--}}
-{{--                    <div class="single-cta">--}}
-{{--                        <i class="far fa-envelope-open"></i>--}}
-{{--                        <div class="cta-text">--}}
-{{--                            <h4>Mail us</h4>--}}
-{{--                            <span>mail@info.com</span>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="footer-content pt-5 pb-5">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-xl-4 col-lg-4 mb-50">--}}
-{{--                    <div class="footer-widget">--}}
-{{--                        <div class="footer-logo">--}}
-{{--                            <a href="index.html"><img src="{{asset('logo/noname.png')}}"--}}
-{{--                                                      class="img-fluid w-20" alt="logo"></a>--}}
-{{--                        </div>--}}
-{{--                        <div class="footer-text">--}}
-{{--                            <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing--}}
-{{--                                elit,Lorem ipsum dolor sit amet.</p>--}}
-{{--                        </div>--}}
-{{--                        <div class="footer-social-icon">--}}
-{{--                            <span>Follow us</span>--}}
-{{--                            <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>--}}
-{{--                            <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>--}}
-{{--                            <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-xl-4 col-lg-4 col-md-6 mb-30">--}}
-{{--                    <div class="footer-widget">--}}
-{{--                        <div class="footer-widget-heading">--}}
-{{--                            <h3>Useful Links</h3>--}}
-{{--                        </div>--}}
-{{--                        <ul>--}}
-{{--                            <li><a href="#">Home</a></li>--}}
-{{--                            <li><a href="#">about</a></li>--}}
-{{--                            <li><a href="#">services</a></li>--}}
-{{--                            <li><a href="#">portfolio</a></li>--}}
-{{--                            <li><a href="#">Contact</a></li>--}}
-{{--                            <li><a href="#">About us</a></li>--}}
-{{--                            <li><a href="#">Our Services</a></li>--}}
-{{--                            <li><a href="#">Expert Team</a></li>--}}
-{{--                            <li><a href="#">Contact us</a></li>--}}
-{{--                            <li><a href="#">Latest News</a></li>--}}
-{{--                        </ul>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-xl-4 col-lg-4 col-md-6 mb-50">--}}
-{{--                    <div class="footer-widget">--}}
-{{--                        <div class="footer-widget-heading">--}}
-{{--                            <h3>Subscribe</h3>--}}
-{{--                        </div>--}}
-{{--                        <div class="footer-text mb-25">--}}
-{{--                            <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>--}}
-{{--                        </div>--}}
-{{--                        <div class="subscribe-form">--}}
-{{--                            <form action="#">--}}
-{{--                                <input type="text" placeholder="Email Address">--}}
-{{--                                <button><i class="fab fa-telegram-plane"></i></button>--}}
-{{--                            </form>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+<footer class="footer-section mt-24">
+    <div class="container">
+        <div class="footer-cta pt-5 pb-5">
+            <div class="row">
+                <div class="col-xl-4 col-md-4 mb-30">
+                    <div class="single-cta">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <div class="cta-text">
+                            <h4>Find us</h4>
+                            <span>1010 Avenue, sw 54321, chandigarh</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4 mb-30">
+                    <div class="single-cta">
+                        <i class="fas fa-phone"></i>
+                        <div class="cta-text">
+                            <h4>Call us</h4>
+                            <span>9876543210 0</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-4 mb-30">
+                    <div class="single-cta">
+                        <i class="far fa-envelope-open"></i>
+                        <div class="cta-text">
+                            <h4>Mail us</h4>
+                            <span>mail@info.com</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-content pt-5 pb-5">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+                            <a href="index.html"><img src="{{asset('logo/noname.png')}}"
+                                                      class="img-fluid w-20" alt="logo"></a>
+                        </div>
+                        <div class="footer-text">
+                            <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
+                                elit,Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <div class="footer-social-icon">
+                            <span>Follow us</span>
+                            <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                            <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                            <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>Useful Links</h3>
+                        </div>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">about</a></li>
+                            <li><a href="#">services</a></li>
+                            <li><a href="#">portfolio</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Our Services</a></li>
+                            <li><a href="#">Expert Team</a></li>
+                            <li><a href="#">Contact us</a></li>
+                            <li><a href="#">Latest News</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>Subscribe</h3>
+                        </div>
+                        <div class="footer-text mb-25">
+                            <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                        </div>
+                        <div class="subscribe-form">
+                            <form action="#">
+                                <input type="text" placeholder="Email Address">
+                                <button><i class="fab fa-telegram-plane"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-{{--    <canvas id="canvas"></canvas>--}}
-{{--    <div class="copyright-area">--}}
-{{--        <div class="container">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-xl-6 col-lg-6 text-center text-lg-left">--}}
-{{--                    <div class="copyright-text">--}}
-{{--                        <p>Copyright &copy; 2018, All Right Reserved <a href="">Anup</a></p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">--}}
-{{--                    <div class="footer-menu">--}}
-{{--                        <ul>--}}
-{{--                            <li><a href="#">Home</a></li>--}}
-{{--                            <li><a href="#">Terms</a></li>--}}
-{{--                            <li><a href="#">Privacy</a></li>--}}
-{{--                            <li><a href="#">Policy</a></li>--}}
-{{--                            <li><a href="#">Contact</a></li>--}}
-{{--                        </ul>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</footer>--}}
+    <canvas id="canvas"></canvas>
+    <div class="copyright-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                    <div class="copyright-text">
+                        <p>Copyright &copy; 2018, All Right Reserved <a href="">Anup</a></p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                    <div class="footer-menu">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Terms</a></li>
+                            <li><a href="#">Privacy</a></li>
+                            <li><a href="#">Policy</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
 {{---<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>--}}
 
